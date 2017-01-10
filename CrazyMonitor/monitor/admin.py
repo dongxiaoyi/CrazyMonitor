f76b53d5a16d8cec909db0bb0950cab4acf6d7c3
dongxy <870428371@qq.com>
2017-01-10 21:15:58 +0800

    告警数据结构注册

--- CrazyMonitor/monitor/admin.py
+++ CrazyMonitor/monitor/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Host)
+admin.site.register(HostGroup)
+admin.site.register(Service)
+admin.site.register(ServiceItem)
+admin.site.register(Action)
+admin.site.register(ActionOperation)
+admin.site.register(Template)
+admin.site.register(Trigger)
+admin.site.register(Maintenance)
